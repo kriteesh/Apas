7c09bf6e0889a61ea1b191e51c53eac0ae6a472b
Kriteesh Parashar <kriteesh.parashar@gmail.com>
2016-07-17 20:09:12 +0530

    Update water_watch.py

--- water_watch.py
+++ water_watch.py
@@ -3,12 +3,15 @@
 #google static maps -generated image #
 # https://maps.googleapis.com/maps/api/staticmap?size=512x512&zoom=15&maptype=satellite&center=Coimbatore&style=feature:water%7Celement:geometry%7Ccolor:0x00ff00%7Cweight:1%7Cvisibility:on&key=AIzaSyBozbn-Zw4R628oOwLB3wfeBvMln8p4ozk#
 # In[24]:
+import cgi
+form = cgi.FieldStorage()
+searchterm =  form.getvalue('searchbox')
 
 from PIL import Image
 import requests
 from io import BytesIO
 
-response = requests.get('http://maps.googleapis.com/maps/api/staticmap?maptype=terrain&size=512x512&zoom=13&center=Delhi&style=feature:water%7Celement:geometry%7Ccolor:0x00ff00%7Cweight:1%7Cvisibility:on&key=AIzaSyBozbn-Zw4R628oOwLB3wfeBvMln8p4ozk')
+response = requests.get('http://maps.googleapis.com/maps/api/staticmap?maptype=terrain&size=512x512&zoom=13&center='+ searchterm +'&style=feature:water%7Celement:geometry%7Ccolor:0x00ff00%7Cweight:1%7Cvisibility:on&key=AIzaSyBozbn-Zw4R628oOwLB3wfeBvMln8p4ozk')
 im = Image.open(BytesIO(response.content))
 
 rgb_im = im.convert('RGB')
